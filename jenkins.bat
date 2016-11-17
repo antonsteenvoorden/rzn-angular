de57aeee3c1b785156e029e78067b2d6764a7c9b
Anton <antons@live.nl>
2016-11-17 14:31:24 +0100

    jenkins script

---
+++ jenkins.bat
@@ -0,0 +1,16 @@
+echo "--- start install ---"
+npm install
+echo "--- end install ---"
+
+echo "--- start lint ---"
+npm run lint
+echo "--- end lint ---"
+
+echo "--- start test ---"
+npm test
+echo "--- end test ---"
+
+echo "--- start build ---"
+npm run build || true
+echo "always succesful for now"
+echo "--- end build ---"
