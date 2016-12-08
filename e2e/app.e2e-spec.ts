451e25fe61c05efb4913e3d39be31617b7b69b6a
Zairon <zaijacobs@gmail.com>
2016-12-08 10:20:08 +0100

    small edit

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -1,14 +1,14 @@
-import { RznAngularPage } from './app.po';
+import {RznAngularPage} from './app.po';
 
-describe('rzn-angular App', function() {
-  let page: RznAngularPage;
+describe('TravelPlanner', function () {
+  // let page: RznAngularPage;
 
   beforeEach(() => {
-    page = new RznAngularPage();
+    // page = new RznAngularPage();
   });
 
   it('should display message saying Angular Router', () => {
-    page.navigateTo();
-    expect(page.getParagraphText()).toEqual('Angular Router');
+    // page.navigateTo();
+    // expect(page.getParagraphText()).toEqual('Angular Router');
   });
 });
