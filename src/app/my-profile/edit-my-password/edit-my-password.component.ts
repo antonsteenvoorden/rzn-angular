63f6d0c3f92f1bffc471dd5ca1c1a673f623e197
Anton <antons@live.nl>
2016-12-15 15:37:34 +0100

    update password

--- src/app/my-profile/edit-my-password/edit-my-password.component.ts
+++ src/app/my-profile/edit-my-password/edit-my-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {ApiService} from "../../services/api.service";
 
 @Component({
   selector: 'app-edit-my-password',
@@ -6,10 +7,22 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['edit-my-password.component.css']
 })
 export class EditMyPasswordComponent implements OnInit {
+  password:string;
+  newPassword:string;
+  repeatPassword:string;
 
-  constructor() { }
+  constructor(private api: ApiService) { }
 
   ngOnInit() {
   }
 
+  updatePassword(){
+    let data = {
+      password:this.password,
+      newPassword:this.newPassword,
+      repeatPassword:this.repeatPassword
+    };
+
+    this.api.put('travellers', data);
+  }
 }
