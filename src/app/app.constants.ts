f3a6253e3b03193c19b109fc254b246a9e05c85e
Anton <antons@live.nl>
2017-01-09 12:15:40 +0100

    test prod diff server urls

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -3,11 +3,13 @@
  */
 
 import {Injectable} from '@angular/core';
+import { environment } from '../environments/environment';
 
 @Injectable()
 export class Configuration {
+  public testServer = (environment.production) ?  'http://localhost:1337/api/' : 'http://localhost:39081/api/';
   // public testServer = 'http://localhost:1337/api/';
-  public testServer = 'http://localhost:39081/api/';
+  // public testServer = 'http://localhost:39081/api/';
   public googleMapsApiKey = 'AIzaSyDArtIeaIwOBi-9UtMlrP36h2kGGwkyS6A';
   public weatherApiKey = 'e4ef7b235ad1d0566245bead6453368c';
 }
