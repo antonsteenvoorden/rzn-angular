ad4434635433171353b60224b10b1cdd3bb8715c
Zairon Jacobs <zaijacobs@gmail.com>
2017-01-11 17:55:52 +0100

    code coverage

--- karma.conf.js
+++ karma.conf.js
@@ -1,6 +1,3 @@
-// Karma configuration file, see link for more information
-// https://karma-runner.github.io/0.13/config/configuration-file.html
-
 module.exports = function (config) {
   config.set({
     basePath: '',
@@ -10,38 +7,61 @@ module.exports = function (config) {
       require('karma-chrome-launcher'),
       require('karma-remap-istanbul'),
       require('angular-cli/plugins/karma'),
-      require('karma-htmlfile-reporter')
+      require('karma-htmlfile-reporter'),
+      require('karma-coverage')
     ],
+
+
     files: [
       {pattern: './src/test.ts', watched: false}
     ],
+
+
     preprocessors: {
-      './src/test.ts': ['angular-cli']
+      './src/test.ts': ['angular-cli'],
+      'src/app/**/*.ts': 'coverage'
     },
+
+
     mime: {
-      'text/x-typescript': ['ts','tsx']
+      'text/x-typescript': ['ts', 'tsx']
     },
+
+
     remapIstanbulReporter: {
       reports: {
         html: 'coverage',
         lcovonly: './coverage/coverage.lcov'
       }
     },
+
+
     angularCli: {
       config: './angular-cli.json',
       environment: 'dev'
     },
-    reporters: ['progress'],
-    htmlReporter: {
-      outputFile: 'tests/units.html',
 
-      // Optional
+
+    reporters: ['progress', 'coverage', 'html'],
+    coverageReporter: {
+      reporters: [{
+        type: 'json',
+        subdir: '.',
+        file: 'coverage-final.json'
+      }]
+    },
+
+
+    htmlReporter: {
+      outputFile: 'report/test/html-report/index.html',
       pageTitle: 'Unit Tests',
       subPageTitle: 'TravelPlanner',
       groupSuites: true,
       useCompactStyle: true,
       useLegacyStyle: true
     },
+
+
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
